c762a29dfcd681cd0e5bbaf18d3d6ca52bdaf4ab
nwils000 <nwils000@gmail.com>
2023-01-21 20:15:55 -0500

    Added array of flashcards

--- src/Components/FlashcardList.jsx
+++ src/Components/FlashcardList.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function FlashcardList() {
+  const [flashcards, setFlashcards] = useState([
+    {
+      question: "What is 2 + 2",
+      answer: "4",
+      options: ["1", "2", "3", "4"],
+    },
+    {
+      question: "Who was the first US president",
+      answer: "George Washington",
+      options: [
+        "Paule Revere",
+        "Abraham Lincoln",
+        "Barack Obama",
+        "George Washington",
+      ],
+    },
+  ]);
   return (
     <>
       <div>
