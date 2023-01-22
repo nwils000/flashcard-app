ef208f95eb5dd1073323fa47caa7e147740315ef
nwils000 <nwils000@gmail.com>
2023-01-21 19:58:18 -0500

    Created jsx for flashcard list

--- src/Components/FlashcardList.jsx
+++ src/Components/FlashcardList.jsx
@@ -1,5 +1,33 @@
 import React from "react";
 
 export default function FlashcardList() {
-  return <div>FlashcardList</div>;
+  return (
+    <>
+      <div>
+        <form>
+          <div>
+            <label htmlFor="category">Category</label>
+            <select id="category">
+              <option value="cateogry title">categories</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="amount">Number of Questions</label>
+            <input
+              type="number"
+              id="amount"
+              min="1"
+              max="99"
+              step="1"
+              defaultValue={10}
+            />
+          </div>
+        </form>
+        <div>
+          <button>Generate</button>
+        </div>
+      </div>
+      <div>{/* Insert flashcards */}</div>
+    </>
+  );
 }
